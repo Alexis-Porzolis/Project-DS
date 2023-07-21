e8e179c248e20aaabac49d0d218a71c66b9ff5d4
Alexis-Porzolis <38305415+Alexis-Porzolis@users.noreply.github.com>
2023-07-21 20:24:40 -0300

    Display of null data

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ from os import system as sys
 sys('cls')
 df = pd.read_csv("MoviesOnStreamingPlatforms.csv", index_col=0)
 
-print(df.count())
+#Creacion de la variable null
+null_count = df.isnull().sum()
+
+#Vizualisar datos nulos
+plt.figure(figsize=(10,6))
+null_count.plot(kind="bar")
+plt.title("Count of null values per column")
+plt.xlabel("Columns")
+plt.ylabel("Number of null values")
+plt.show()
