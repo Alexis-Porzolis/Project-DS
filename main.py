c2c26090970f5cd99fa311530d9a830aef49ff49
Alexis-Porzolis <38305415+Alexis-Porzolis@users.noreply.github.com>
2023-07-21 21:03:03 -0300

    Creation of age percentage variable

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ sys('cls')
 df = pd.read_csv("MoviesOnStreamingPlatforms.csv", index_col=0)
 
 #Creacion de la variable null
-null_count = df.isnull().sum()
+null_counts = df.isnull().sum()
 
 #Vizualisar datos nulos
 plt.figure(figsize=(10,6))
-null_count.plot(kind="bar")
+null_counts.plot(kind="bar")
 plt.title("Count of null values per column")
 plt.xlabel("Columns")
 plt.ylabel("Number of null values")
-plt.show()
+
+
+#Creacion de la variable null porcentajes
+
+null_percentages = (df["Age"].isnull().sum()/len(df["Age"]))*100
+print (f"Percentage of null values\nEdad:{null_percentages:.2f}%")
