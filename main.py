4212648e9ab0bd432c4e4cca1812202ffc66fc34
Alexis-Porzolis <38305415+Alexis-Porzolis@users.noreply.github.com>
2023-07-21 22:14:38 -0300

    Creation of function optimizing the previous null data

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 from os import system as sys
 
 sys('cls')
-df = pd.read_csv("MoviesOnStreamingPlatforms.csv", index_col=0)
-
+df = pd.read_csv("MoviesOnStreamingPlatforms.csv", index_col="ID", na_values="#N/D")
+values_ascending =df.isna().sum().sort_values(ascending=False)
+print(values_ascending)
 #Creacion de la variable null
 null_counts = df.isnull().sum()
 
@@ -15,9 +16,13 @@ null_counts.plot(kind="bar")
 plt.title("Count of null values per column")
 plt.xlabel("Columns")
 plt.ylabel("Number of null values")
+plt.show()
 
+#Creamos la funcion nullpercentage
+def nullpercentage (df):
+    null = (df.isnull().sum()/len(df))*100
+    print (f"Percentage of null values\n{null}%")
 
-#Creacion de la variable null porcentajes
-
-null_percentages = (df["Age"].isnull().sum()/len(df["Age"]))*100
-print (f"Percentage of null values\nEdad:{null_percentages:.2f}%")
+#Usamos la funcion
+nullpercentage(df["Age"])
+nullpercentage(df["Rotten Tomatoes"])
